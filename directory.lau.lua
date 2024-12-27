3ed203977a9b21bcd650a2318f9c28e50b9afe2e
AravindaDev1 <prabhasha.aravinda@gmail.com>
2024-12-27 19:47:15 +0530

    Add files via upload

---
+++ directory.lau.lua
@@ -0,0 +1,15 @@
+/coding-competition
+|-- index.html         (Home Page)
+|-- timeline.html      (Timeline Page)
+|-- shop.html          (Shop Page)
+|-- contact.html       (Contact Us Page)
+|-- style.css          (Shared CSS File)
+|--images/
+    └── background1.jpg/
+    
+    
+
+   
+    
+
+
